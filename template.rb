6a0109e00635bc00b02403d6abf03d95d6e3b121
Kyo Yamada <hysteric.madam@gmail.com>
2015-09-19 14:20:21 +0900

    2個以上改行が続いているところを削除

--- template.rb
+++ template.rb
@@ -7,7 +7,7 @@ run 'gibo OSX Ruby Rails Vim SublimeText > .gitignore' rescue nil
 
 # Gemfile
 gsub_file 'Gemfile', /#.+\n/, ''
-gsub_file 'Gemfile', /gem 'jquery-rails'\n/, ''
+gsub_file 'Gemfile', /\n\{2,}/, ''
 
 gem_group :default do
   gem 'slim-rails'
