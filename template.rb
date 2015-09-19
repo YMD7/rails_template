6ede9eeb1146ced48359e8f6b387ef3df090c53d
Kyo Yamada <hysteric.madam@gmail.com>
2015-09-19 14:52:55 +0900

    改行コードを修正

--- template.rb
+++ template.rb
@@ -7,7 +7,7 @@ run 'gibo OSX Ruby Rails Vim SublimeText > .gitignore' rescue nil
 
 # Gemfile
 gsub_file 'Gemfile', /#.+\n/, ''
-gsub_file 'Gemfile', /^$\n{2,}/, /\n/
+gsub_file 'Gemfile', /^$\n{2,}/, "\n"
 
 gem 'unicorn'
 gem 'devise'
