c4cd3743300311d0ca7b7a3bc038b95cadbfd639
Kyo Yamada <hysteric.madam@gmail.com>
2015-09-19 14:47:29 +0900

    改行の表記修正とgemの追加

--- template.rb
+++ template.rb
@@ -7,8 +7,17 @@ run 'gibo OSX Ruby Rails Vim SublimeText > .gitignore' rescue nil
 
 # Gemfile
 gsub_file 'Gemfile', /#.+\n/, ''
-gsub_file 'Gemfile', /^$\n{2,}/, \n
+gsub_file 'Gemfile', /^$\n{2,}/, /\n/
 
-gem_group :default do
-  gem 'slim-rails'
+gem 'unicorn'
+gem 'devise'
+gem 'slim-rails'
+gem 'bourbon'
+aem 'neat'
+
+group :development, :test do
+  gem 'pry-rails'
+  gem 'pry-doc'
+  gem 'pry-byebug'
+  gem 'pry-stack_explorer'
 end
