370a7837e63f4da6275a7ee6fccf06efbfafa273
Kyo Yamada <hysteric.madam@gmail.com>
2015-09-19 14:15:27 +0900

    gsub_file テスト

--- template.rb
+++ template.rb
@@ -7,6 +7,7 @@ run 'gibo OSX Ruby Rails Vim SublimeText > .gitignore' rescue nil
 
 # Gemfile
 gsub_file 'Gemfile', /#.+\n/, ''
+gsub_file 'Gemfile', /gem 'jquery-rails'\n/, ''
 
 gem_group :default do
   gem 'slim-rails'
