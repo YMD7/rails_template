2669fb900b8f054ff9d0a65056d1113cb563ee7a
Kyo Yamada <hysteric.madam@gmail.com>
2015-09-19 22:10:24 +0900

    その他のテンプレートを追記（参考：https://github.com/kadoppe/rails-template/blob/master/template.rb）

--- template.rb
+++ template.rb
@@ -15,9 +15,28 @@ gem 'slim-rails'
 gem 'bourbon'
 gem 'neat'
 
+gem_group :development do
+  gem 'html2slim'
+end
+
 gem_group :development, :test do
   gem 'pry-rails'
   gem 'pry-doc'
   gem 'pry-byebug'
   gem 'pry-stack_explorer'
 end
+
+# install gems
+run 'bundle install'
+
+# convert erb file to slim
+run 'bundle exec erb2slim -d app/views'
+
+# DB migration
+rake 'db:migrate'
+
+# git
+git :init
+git add: '.'
+git commit: "-m 'Initial commit'"
+
