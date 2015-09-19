1c9a83fc81f71e8cb9a980af61e5039e99b6b516
YMD7 <hysteric.madam@gmail.com>
2015-09-19 14:29:07 +0900

    Update template.rb

--- template.rb
+++ template.rb
@@ -7,7 +7,7 @@ run 'gibo OSX Ruby Rails Vim SublimeText > .gitignore' rescue nil
 
 # Gemfile
 gsub_file 'Gemfile', /#.+\n/, ''
-gsub_file 'Gemfile', /^$\n{2,}/, 'hoge'
+gsub_file 'Gemfile', /^$\n{2,}/, '\n'
 
 gem_group :default do
   gem 'slim-rails'
