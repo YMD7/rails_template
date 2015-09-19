e947f94b03a9f2a8e21217331be956e356a732f3
Kyo Yamada <hysteric.madam@gmail.com>
2015-09-19 14:12:30 +0900

    コメント行削除の正規表現を修正

--- template.rb
+++ template.rb
@@ -6,8 +6,7 @@
 run 'gibo OSX Ruby Rails Vim SublimeText > .gitignore' rescue nil
 
 # Gemfile
-
-gsub_file 'Gemfile', /#\w+\n/, ''
+gsub_file 'Gemfile', /#.+\n/, ''
 
 gem_group :default do
   gem 'slim-rails'
