7c2a51efba2e4f4899107c7ae442a9cf73aed6bb
Kyo Yamada <hysteric.madam@gmail.com>
2015-09-22 12:56:40 +0900

    unicornの設定ファイルとrakeファイルを追加

--- template.rb
+++ template.rb
@@ -150,7 +150,67 @@ VARIABLES
 # DB migration
 rake 'db:migrate'
 
-# design folder
+# lib/tasks/unicorn.rake
+create_file 'lib/tasks/unicorn.rake', <<RAKE
+namespace :unicorn do
+  ##
+  # Tasks
+  ##
+  desc "Start unicorn for development env."
+  task(:start) {
+    config = Rails.root.join('config', 'unicorn.rb')
+    sh "bundle exec unicorn_rails -c #{config} -E development -D"
+  }
+
+  desc "Stop unicorn"
+  task(:stop) { unicorn_signal :QUIT }
+
+  desc "Restart unicorn with USR2"
+  task(:restart) { unicorn_signal :USR2 }
+
+  desc "Increment number of worker processes"
+  task(:increment) { unicorn_signal :TTIN }
+
+  desc "Decrement number of worker processes"
+  task(:decrement) { unicorn_signal :TTOU }
+
+  desc "Unicorn pstree (depends on pstree command)"
+  task(:pstree) do
+    sh "pstree '#{unicorn_pid}'"
+  end
+
+  def unicorn_signal signal
+    Process.kill signal, unicorn_pid
+  end
+
+  def unicorn_pid
+    begin
+      File.read("tmp/unicorn.pid").to_i
+    rescue Errno::ENOENT
+      raise "Unicorn doesn't seem to be running"
+    end
+  end
+
+end
+RAKE
+
+# config/unicorn.rb
+create_file 'config/unicorn.rb', <<UNICORN
+# -*- coding: utf-8 -*-
+rails_root = File.expand_path('../../', __FILE__)
+
+worker_processes 2
+working_directory rails_root
+
+listen "#{rails_root}/tmp/unicorn.sock"
+pid "#{rails_root}/tmp/unicorn.pid"
+
+stderr_path "#{rails_root}/log/unicorn_error.log"
+stdout_path "#{rails_root}/log/unicorn.log"
+
+UNICORN
+
+# make design folder
 Dir.mkdir 'design'
 
 # remove files
